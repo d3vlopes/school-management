d87baacd18b2d362fe4a01be8554ef75ad5c309e
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-05-03 14:32:35 -0300

    feat: add httpHelper

---
+++ src/presentation/helpers/httpHelper.ts
@@ -0,0 +1,37 @@
+import { ServerError } from '@/presentation/errors'
+
+import { IHttpResponse } from '@/presentation/contracts'
+
+export const badRequest = (error: Error): IHttpResponse => {
+  return {
+    statusCode: 400,
+    body: error,
+  }
+}
+
+export const ok = (data: Record<string, unknown>): IHttpResponse => ({
+  statusCode: 200,
+  body: data,
+})
+
+export const created = (
+  data: Record<string, unknown>,
+): IHttpResponse => ({
+  statusCode: 201,
+  body: data,
+})
+
+export const serverError = (message?: string): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(message),
+})
+
+export const forbidden = (error: Error): IHttpResponse => ({
+  statusCode: 403,
+  body: error,
+})
+
+export const noContent = (): IHttpResponse => ({
+  statusCode: 204,
+  body: null,
+})
