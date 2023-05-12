9c91cafc2d050a07574b412cd511ee99d775320e
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-05-12 14:47:25 -0300

    feat: add route for get admin profile on adminRoutes

--- src/main/routes/adminRoutes.ts
+++ src/main/routes/adminRoutes.ts
@@ -5,7 +5,10 @@ import { adaptRoute } from '@/infra/adapters/http/express/adaptRoute'
 import {
   makeAdminRegisterController,
   makeAdminLoginController,
+  makeAdminGetProfileController,
 } from '@/main/factories/controllers/modules/admin'
+
+import { authMiddleware } from '@/main/middlewares'
 
 export const usersRoutes = Router()
 
@@ -16,4 +19,10 @@ export default (router: Router): void => {
   )
 
   router.post('/admin/login', adaptRoute(makeAdminLoginController()))
+
+  router.get(
+    '/admin/profile',
+    authMiddleware,
+    adaptRoute(makeAdminGetProfileController()),
+  )
 }
