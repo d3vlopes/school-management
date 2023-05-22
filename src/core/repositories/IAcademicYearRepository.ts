2db0b182c4ded95bb57dbc35919b4ae4bf6255e0
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-05-22 15:16:54 -0300

    feat: add academicYearCreate controller

--- src/core/repositories/IAcademicYearRepository.ts
+++ src/core/repositories/IAcademicYearRepository.ts
@@ -1,9 +1,11 @@
 import { AcademicYearModel } from '@/core/models'
-import { AcademicYearCreateDTO } from '@/core/dtos/academicYear'
+import { AcademicYearCreateRequestDTO } from '@/core/dtos/academicYear'
 
 export interface IAcademicYearRepository {
   findOne: (
     data: Partial<AcademicYearModel>,
   ) => Promise<AcademicYearModel | null>
-  create(data: AcademicYearCreateDTO): Promise<AcademicYearModel>
+  create(
+    data: AcademicYearCreateRequestDTO,
+  ): Promise<AcademicYearModel>
 }
---
+++ src/presentation/controllers/modules/academicYear/create/mappers/index.ts
@@ -0,0 +1,19 @@
+import { AcademicYearCreateRequestDTO } from '@/core/dtos/academicYear'
+import { AcademicYearModel } from '@/core/models'
+
+import { IMapper } from '@/presentation/contracts'
+
+class AcademicYearCreateMapper
+  implements IMapper<AcademicYearModel, AcademicYearCreateRequestDTO>
+{
+  toDTO(model: AcademicYearModel): AcademicYearCreateRequestDTO {
+    return {
+      id: model.id,
+      name: model.name,
+      year: model.year,
+      createdBy: model.createdBy,
+    }
+  }
+}
+
+export const academicYearCreateMapper = new AcademicYearCreateMapper()
