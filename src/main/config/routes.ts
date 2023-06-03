8b62748a32c89d7abfb9aebc29747ca4a3212283
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-03 00:50:25 -0300

    refactor: routes structure

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,17 +1,11 @@
 import { Express, Router } from 'express'
 
-import {
-  adminRoutes,
-  academicTermRoutes,
-  academicYearRoutes,
-} from '../routes'
+import { routes } from '../routes'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
 
-  router.use(adminRoutes)
-  router.use(academicYearRoutes)
-  router.use(academicTermRoutes)
+  router.use(routes)
 }
---
+++ src/main/routes/modules/index.ts
@@ -0,0 +1,3 @@
+export * from './adminRoutes'
+export * from './academicYearRoutes'
+export * from './academicTermRoutes'
