b60aa8568fc0f32c8d539b2a2c6b84c718ce4087
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-05 19:25:21 -0300

    feat: add academicTermUpdate use case

--- src/useCases/modules/academicTerm/index.ts
+++ src/useCases/modules/academicTerm/index.ts
@@ -1,3 +1,4 @@
 export * from './create/academicTermCreateUseCase'
 export * from './getAll/academicTermGetAllUseCase'
 export * from './getById/academicTermGetByIdUseCase'
+export * from './update/academicTermUpdateUseCase'
