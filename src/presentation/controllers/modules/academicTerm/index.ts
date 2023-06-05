a37c72d7bef635875728949995598cd289df82a2
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-05 19:31:28 -0300

    feat: add academicTermUpdate controller

--- src/presentation/controllers/modules/academicTerm/index.ts
+++ src/presentation/controllers/modules/academicTerm/index.ts
@@ -1,3 +1,4 @@
 export * from './create/academicTermCreateController'
 export * from './getAll/academicTermGetAllController'
 export * from './getById/academicTermGetByIdController'
+export * from './update/academicTermUpdateController'
