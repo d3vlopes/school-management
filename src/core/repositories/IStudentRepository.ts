140429ae461303c88eb3e27169d307cc9e613adc
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-14 19:43:20 -0300

    feat: add StudentRepository

---
+++ src/core/repositories/IStudentRepository.ts
@@ -0,0 +1,9 @@
+import { StudentModel } from '@/core/models'
+import { StudentRegisterRequestDTO } from '@/core/dtos/student'
+
+export interface IStudentRepository {
+  create(
+    data: StudentRegisterRequestDTO & { studentId: string },
+  ): Promise<StudentModel>
+  findOne(data: Partial<StudentModel>): Promise<StudentModel | null>
+}
