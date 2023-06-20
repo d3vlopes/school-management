ded9488186998b3ac8007423723301567cb73ba2
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-20 19:49:47 -0300

    feat: add route for docs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,14 @@
 import { Express, Router } from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import { routes } from '../routes'
 
+import swaggerFile from '../../docs/swagger-output.json'
+
 export const setupRoutes = (app: Express): void => {
   const router = Router()
+
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
   app.use('/api', router)
 
