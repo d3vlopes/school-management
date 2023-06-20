766f5551dc4b63e47cd02440ac154a277e207f40
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-06-20 19:49:03 -0300

    chore: add swagger autogen config

---
+++ src/docs/index.ts
@@ -0,0 +1,18 @@
+export const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'School Manager API',
+    description: 'Documentation of School Manager API',
+  },
+  host: 'localhost:8000/api',
+  basePath: '/',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'Admin',
+      description: 'Endpoint for administration',
+    },
+  ],
+}
---
+++ src/main/config/swagger.ts
@@ -0,0 +1,14 @@
+import swaggerAutogen from 'swagger-autogen'
+
+import { doc } from '@/docs'
+
+const outputFile = '../../docs/swagger-output.json'
+const endpointsFiles = ['../routes/modules/*Routes.ts']
+
+swaggerAutogen({ openapi: '3.0.0' })(
+  outputFile,
+  endpointsFiles,
+  doc,
+).then(async () => {
+  await import('../../infra/server/index')
+})
